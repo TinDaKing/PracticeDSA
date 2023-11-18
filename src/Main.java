4c8824fe8d224e7617471ffe53158b7c135d246a
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-11-18 15:43:38 +0700

    tryin to find the answer

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int A[] = {4,5,6,7,3,4,1,5};
+        int A[] = {1,2,3,3,2,1,5};
         Solution solution = new Solution();
-        System.out.println("Result: " + solution.solution1(A));
+        System.out.println("\nResult: " + solution.solution1(A));
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
 public class Solution{
-    public int solution1(int[] A){
+    public int test1(int[] A){
         Integer result = 0;
         int n= A.length;
         if((n&1)==1){
@@ -10,6 +10,42 @@ public class Solution{
             result = result + A[i] - A[++i];
         }
         return result;
+    }
+
+    public int solution1(int[]A){
+        Integer result=0;
+        Integer tempResult=0;
+        int loop=0;
+        int minusTime = 0;
+        while (loop<A.length){
+            for (int i=loop; i<A.length;i++){
+                int storageA[]= new int[A.length];
+                tempResult += A[i];
+                System.out.print(A[i]+" ");
+                for (int j=i+1; j<A.length; j++){
+                    if(A[i] > A[j]) {
+                        tempResult -= A[j];
+                        System.out.println(A[j]+" ");
+                        minusTime++;
+                        i = j;
+                        break;
+                    }
+                }
+                if (minusTime==0){
+                    tempResult-=A[i];
+                    tempResult+=A[A.length-1];
+                    i = A.length;
+                }
+                minusTime = 0;
+            }
+            if (tempResult > result){
+                result = tempResult;
+                System.out.println("End of loop " + loop);
+            }
+            tempResult = 0;
+            loop++;
+        }
+        return result % 1000000000;
     }
 
 }
