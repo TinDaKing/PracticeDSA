603f3417af042a5570939b1e51ff3b56bf3a1048
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-11-19 02:17:16 +0700

    done problem kkk

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        int A[] = {1,2,3,3,2,1,5};
+        int A[] = {4,1,2,3};
+        int A1[] = {1,2,3,3,2,1,5};
+        int A2[] = {1000000000,1,2,2,1000000000,1,1000000000};
+        int A3[] = {4,5,6,7,3,4,1,5};
+        int A4[] = {8,8,8,8};
+        int A5[] = {1,2,3,4,5};
+        int A6[] = {5,4,3,2,1};
         Solution solution = new Solution();
-        System.out.println("\nResult: " + solution.solution1(A));
+        System.out.println("\nResult: " + solution.solution2(A6));
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution{
     public int test1(int[] A){
         Integer result = 0;
@@ -15,6 +19,7 @@ public class Solution{
     public int solution1(int[]A){
         Integer result=0;
         Integer tempResult=0;
+        Integer minusElement=0;
         int loop=0;
         int minusTime = 0;
         while (loop<A.length){
@@ -24,16 +29,15 @@ public class Solution{
                 System.out.print(A[i]+" ");
                 for (int j=i+1; j<A.length; j++){
                     if(A[i] > A[j]) {
-                        tempResult -= A[j];
+                        minusElement = A[j];
                         System.out.println(A[j]+" ");
                         minusTime++;
                         i = j;
-                        break;
                     }
                 }
-                if (minusTime==0){
-                    tempResult-=A[i];
-                    tempResult+=A[A.length-1];
+                if (minusTime == 0){
+                    tempResult -= A[i];
+                    tempResult += A[A.length-1];
                     i = A.length;
                 }
                 minusTime = 0;
@@ -48,4 +52,78 @@ public class Solution{
         return result % 1000000000;
     }
 
+    public int solution2(int[] A){
+        List<Integer> newA = cleanArray(A);
+        if(newA.size() == 1){
+            return newA.get(0);
+        }
+        BigInteger result = BigInteger.valueOf(0);
+        int countContinuousStreak = 0;
+        // 3 continuous ascending numbers: 3
+        // 3 continuous descending numbers: -3
+
+        int startStreakIndex = 0;
+        System.out.println(newA);
+
+        for(int i=1; i< newA.size(); i++){
+            if(newA.get(i) > newA.get(i-1) && countContinuousStreak >= 0){
+                countContinuousStreak++;
+                System.out.println("Up streak "+countContinuousStreak+": "+newA.get(i));
+            } else if (newA.get(i) < newA.get(i-1) && countContinuousStreak <= 0) {
+                countContinuousStreak--;
+                System.out.println("Down streak "+countContinuousStreak+": "+newA.get(i));
+            } else {
+                System.out.println("Change streak "+countContinuousStreak+": "+newA.get(i));
+                if (i < newA.size()-1 && countContinuousStreak >0){
+                    countContinuousStreak = -1;
+                }
+                else if (i < newA.size()-1 && countContinuousStreak <0) {
+                    result = result.add(BigInteger.valueOf(descendingPart(newA.subList(startStreakIndex,i))));
+                    result = result.subtract(BigInteger.valueOf(newA.get(i-1)));
+                    countContinuousStreak = 1;
+
+                } else if (i==newA.size()-1 && countContinuousStreak >0) {
+                    result = result.add(BigInteger.valueOf(ascendingPart(newA.subList(startStreakIndex,i))));
+                    countContinuousStreak = 0;
+                    break;
+
+                } else if(i==newA.size()-1 && countContinuousStreak <0){
+                    System.out.println(i);
+                    result = result.add(BigInteger.valueOf(descendingPart(newA.subList(startStreakIndex,i))));
+                    System.out.println(descendingPart(newA.subList(startStreakIndex,i))+" "+newA.get(i-1)+" "+newA.get(i));
+                    result = result.subtract(BigInteger.valueOf(newA.get(i-1)));
+                    result = result.add(BigInteger.valueOf(newA.get(i)));
+                    countContinuousStreak = 0;
+                }
+                System.out.println("Current result: "+result.intValue()+"\n");
+                startStreakIndex = i-1;
+            }
+
+        }
+
+        if(countContinuousStreak >0){
+            result = result.add(BigInteger.valueOf(ascendingPart(newA.subList(startStreakIndex, newA.size()))));
+        } else if(countContinuousStreak <0){
+            result = result.add(BigInteger.valueOf(descendingPart(newA.subList(startStreakIndex,newA.size()))));
+
+        }
+        return result.mod(BigInteger.valueOf(1000000000)).intValue();
+    }
+    public List<Integer> cleanArray(int[] A){ //eliminate the nearby same number
+        List<Integer> newA = new ArrayList<>();
+        newA.add(A[0]);
+        for(int i=1; i<A.length; i++){
+            if(A[i] != A[i-1]){
+                newA.add(A[i]);
+            }
+        }
+        return newA;
+    }
+    public int ascendingPart(List<Integer> A) {
+        return A.get(A.size()-1);
+    }
+
+    public int descendingPart(List<Integer> A){
+        return A.get(0);
+    }
 }
