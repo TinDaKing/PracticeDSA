7197b49abedf6ae7896d47470a5984b3f66c75b2
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-11-22 00:00:23 +0700

    max profit calculating

--- src/week02/MaxProfit.java
+++ src/week02/MaxProfit.java
@@ -2,6 +2,10 @@ package week02;
 
 public class MaxProfit {
     public int solution(int[] A){
-
+        int dayBuy = 0;
+        int daySell = 1;
+        for(int i=0;i<A.length;i++){
+            if()
+        }
     }
 }
