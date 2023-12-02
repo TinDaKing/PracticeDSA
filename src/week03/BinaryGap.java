dc1ea5afe1e618f0540ee8199b1e7ce81ad25c82
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-02 11:09:57 +0700

    binary gap refactor

--- src/week03/BinaryGap.java
+++ src/week03/BinaryGap.java
@@ -2,15 +2,13 @@ package week03;
 
 public class BinaryGap {
     public static int solution(int N) {
+        System.out.println(N+" to binary: "+ Integer.toBinaryString(N));
+
         int maxBinaryGap = 0;
         int count = 0;
-        String binaryN = Integer.toBinaryString(N);
-        System.out.println(N+" to String: "+binaryN);
-        for (int i = 0; i < binaryN.length(); i++) {
-            if (binaryN.charAt(i) == '1') {
-                if (count > maxBinaryGap) {
-                    maxBinaryGap=count;
-                }
+        for (char c : Integer.toBinaryString(N).toCharArray()) {
+            if (c == '1') {
+                maxBinaryGap = Math.max(maxBinaryGap, count);
                 count = 0;
             } else {
                 count++;
