0f1d5c884797510735c038c20f8cf2b8968368bb
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-02 20:48:14 +0700

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ public class Main {
     public static void main(String[] args) {
 
         // week 3 binary gap
-        int N=9;
-        int N1=529;
-        int N2=15;
-        int N3=32;
+        int N = 9;
+        int N1 = 529;
+        int N2 = 15;
+        int N3 = 32;
 
-        System.out.println("Binary gap: " +BinaryGap.getInstance().solution(N1));
+        System.out.println("Binary gap: " + BinaryGap.getInstance().solution(N1));
 
         // week 3 primes simples
-        System.out.println("Prime amount: "+PrimesSimple.getInstance().solution(11,19));
+        System.out.println("Prime amount: " + PrimesSimple.getInstance().solution(11, 19));
 
         // week 3 missing integer
-        int A[]={1,3,6,4,1,2};
-        int A1[]={1,2,3};
-        int A2[]={-1,-3};
-        System.out.println("Missing integer: "+MissingInteger.getInstance().solution(A));
+        int A[] = {1, 3, 6, 4, 1, 2};
+        int A1[] = {1, 2, 3};
+        int A2[] = {-1, -3};
+        System.out.println("Missing integer: " + MissingInteger.getInstance().solution(A));
 
     }
 
