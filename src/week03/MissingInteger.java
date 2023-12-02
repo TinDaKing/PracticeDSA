112ed7865b57694b1f4db74200fd2af7d322bc36
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-02 17:45:12 +0700

    i was lost

---
+++ src/week03/MissingInteger.java
@@ -0,0 +1,23 @@
+package week03;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingInteger {
+    private static MissingInteger instance;
+    private MissingInteger(){};
+
+    public static MissingInteger getInstance() {
+        if (instance == null) {
+            instance = new MissingInteger();
+        }
+        return instance;
+    }
+    public int solution(int A[]){
+        Set<Integer> numberInArray = new HashSet<>();
+        int result = 0;
+        for(int a: A){
+
+        }
+    }
+}
